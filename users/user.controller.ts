42c1338f7d9d8aad17f8d4e3301e6213f7797f5f
khaalidsub <khaalidsubaan@gmail.com>
2021-10-02 23:10:35 +0800

    Add prisma client

--- users/user.controller.ts
+++ users/user.controller.ts
@@ -1,7 +1,15 @@
 import {Request,Response} from 'express'
+import * as usersService from './user.service'
+export const createUser =async (request: Request, response: Response) => {
 
-export const createUser = (request: Request, response: Response) => {}
-export const getUsers = (request: Request, response: Response) => {}
+    const newUser = await usersService.createUser(request.body)
+    console.log(newUser);
+    return response.json(newUser)
+}
+export const getUsers = async(request: Request, response: Response) => {
+    const users = await usersService.getUsers()
+    return response.json(users)
+}
 export const getOneUser = (request: Request, response: Response) => {}
 export const updateUser = (request: Request, response: Response) => {}
 export const deleteUser = (request: Request, response: Response) => {}
--- users/user.service.ts
+++ users/user.service.ts
@@ -1,5 +1,10 @@
-export const createUser = (userDTO:any) => {}
-export const getUsers = () => {}
+import {prisma} from '../config'
+export const createUser = (userDTO:any) => {
+    return prisma.user.create({data:userDTO})
+}
+export const getUsers = () => {
+    return prisma.user.findMany()
+}
 export const getOneUser = (id:string) => {}
 export const updateUser = (userDTO:any) => {}
 export const deleteUser = (id:string) => {}
