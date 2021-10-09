3f978d51c5ab0cb0a4f67ce47fa5abcddcb61191
khaalidsub <khaalidsubaan@gmail.com>
2021-10-10 00:13:55 +0800

    Add one to many

--- books/book.controller.ts
+++ books/book.controller.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express'
+import * as booksService from './book.service'
+export const createBook = async (request: Request, response: Response) => { 
 
-export const createBook = (request: Request, response: Response) => { }
-export const getBooks = (request: Request, response: Response) => { }
+    const newBook = await booksService.createBook(request.body)
+    console.log(newBook);
+    return response.json(newBook);
+}
+export const getBooks = async(request: Request, response: Response) => { 
+
+    const books = await booksService.getBooks()
+    return response.json(books)
+}
 export const getOneBook = (request: Request, response: Response) => { }
 export const updateBook = (request: Request, response: Response) => { }
 export const deleteBook = (request: Request, response: Response) => { }
--- books/book.service.ts
+++ books/book.service.ts
@@ -1,5 +1,10 @@
-export const createBook = (BookDTO: any) => { }
-export const getBooks = () => { }
+import { prisma } from '../config'
+export const createBook = (bookDTO: any) => {
+    return prisma.book.create({ data: bookDTO, include: { author: true } })
+}
+export const getBooks = () => { 
+    return prisma.book.findMany({include: { author:true}})
+}
 export const getOneBook = (id: string) => { }
-export const updateBook = (BookDTO: any) => { }
+export const updateBook = (bookDTO: any) => { }
 export const deleteBook = (id: string) => { }
